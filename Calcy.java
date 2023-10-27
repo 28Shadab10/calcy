af5d1ebe69bd21ab0cf7c34e9d492eee4044ffdf
28shadab10 <mdshadabalamgrd10@gmail.com>
2023-10-27 14:34:02 +0530

    Calculator Body

---
+++ Calcy.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calcy{
+
+    public static void main(String[] args){
+
+        int a;
+        int b;
+        int c;
+        int cal;
+        int remainder;
+        int quotient;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "Enter 2 Numbers :- ");
+        System.out.println( "Enter First Number :- ");
+        a = sc.nextInt();
+        System.out.println("Enter Second Number :- ");
+        b = sc.nextInt();
+
+        System.out.println("Select the operation :- ");
+        System.out.println("Enter 1 for Sum + ");
+        System.out.println("Enter 2 for Substraction - ");
+        System.out.println("Enter 3 for Division / ");
+        System.out.println("Enter 4 for Multiplication *");
+        int input;
+        input = sc.nextInt();
+
+        if(input == 1){
+        // operation for Sum +
+      
+        }
+        else if(input == 2){
+        // operation for Substraction - 
+    
+        }else if(input == 3){
+        // operation for Division / 
+        }
+        else if (input == 4){
+        // operation for Multiplication *
+
+        }
+        else{
+            System.out.println("Enter valid input");
+        }
+    }
+
+}
